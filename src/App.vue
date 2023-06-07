49b4d712a3d61b76010173ad2a7986b438358fee
joe-no-body <20505941+joe-no-body@users.noreply.github.com>
2023-06-07 05:41:03 -0700

    clean up unused items

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,13 @@
 import { ref } from 'vue';
 import PromptInput from './components/PromptInput.vue';
 import AppHeader from './components/AppHeader.vue';
-import { useClock } from './utils/time.js';
 
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 
 const userSelection = ref('');
 const message = ref(null);
 
 const displayTitle = ref('DIGITAL PUBLIC LIBRARY');
-
-const {date, time} = useClock();
 
 function handleUserSelectionValue(value) {
   message.value = `Invalid selection: ${userSelection.value}`;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref } from 'vue';
 
 const msg = ref('Hello World!');
 </script>
